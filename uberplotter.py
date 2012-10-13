0f8ecdb1a87a1265d635ee4e9fc15719da120c10
femnad <femnad@gmail.com>
2012-10-13 21:37:59 +0300

    Improve uberplotter

--- uberplotter.py
+++ uberplotter.py
@@ -9,7 +9,8 @@ OLD_FILES_DIRECTORY = "what-once-was"
 NUMBER_OF_EPOCHS = 10000
 LEGEND_POSITION = 3
 
-START_INDEX = 1
+START_INDEX = 0
+STOP_INDEX = 5
 
 processed_files = []
 
@@ -48,12 +49,14 @@ def read_files(file_list):
         if line[0] is not '#':
             pairs_and_info = line.split('|')
             file_pairs = pairs_and_info[0].split(';')
-            a_pair = []
-            for single_file in file_pairs:
-                processed_files.append(single_file)
-                a_pair.append(get_float_values_from_file(single_file))
-            info = json.loads(pairs_and_info[1])
-            plot_data(a_pair, info)
+            for i in range(START_INDEX,STOP_INDEX+1):
+                a_pair = []
+                for single_file in file_pairs:
+                    single_file_name = single_file.format(i)
+                    processed_files.append(single_file_name)
+                    a_pair.append(get_float_values_from_file(single_file_name))
+                info = json.loads(pairs_and_info[1])
+                plot_data(a_pair, info, i)
     input_file_list.close()
 
 def get_float_values_from_file(file_name):
@@ -64,8 +67,8 @@ def get_float_values_from_file(file_name):
     file_obj.close()
     return values
 
-def plot_data(value_pairs, info):
-    global START_INDEX
+def plot_data(value_pairs, info, sim_number):
+    print "Plotting simulation number: {0:d}".format(sim_number)
     for index, values in enumerate(value_pairs):
         plt.plot(range(1,1+NUMBER_OF_EPOCHS), values, c=info['colors'][index],
                  label=info['labels'][index])
@@ -74,9 +77,8 @@ def plot_data(value_pairs, info):
     plt.ylabel(info['ylabel'])
     if info.has_key('ylimits'):
         plt.ylim(info['ylimits'][0], info['ylimits'][1])
-    plt.savefig("{0:02d}-output.png".format(START_INDEX))
+    plt.savefig("output-{0:02d}.png".format(sim_number))
     plt.clf()
-    START_INDEX += 1
 
 def cleanup(files):
     print "Rename old files? y/n"
