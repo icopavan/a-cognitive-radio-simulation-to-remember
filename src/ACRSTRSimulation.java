9053bbfbc24b03f42d0103f383db7e005a860e21
femnad <femnad@gmail.com>
2013-01-02 13:10:39 +0200

    General cleanup

--- src/ACRSTRSimulation.java
+++ src/ACRSTRSimulation.java
@@ -28,11 +28,11 @@ public class ACRSTRSimulation {
 	public static final int START_N_VALUES = 0;
 	public static final int END_N_VALUES = 0;
 	
-	public static List<QValuesResponse> qValuesResponses;
-	public static List<RatesResponse> ratesResponses;
-	public static List<Method> methodsToSimulate;
-	public static List<Integer> lastValuesToCheck;
-	public static List<String> epsilonDecrements;
+	public QValuesResponse qValuesResponse;
+	public RatesResponse ratesResponse;
+	public Method methodToSimulate;
+	public Integer lastValuesToCheck;
+	public String epsilonDecrement;
 	
 	public static Stack<String> colors;
 	
@@ -46,17 +46,11 @@ public class ACRSTRSimulation {
 		colors.push("blue");
 		colors.push("red");
 				
-		lastValuesToCheck = new ArrayList<Integer>();
-		qValuesResponses = new ArrayList<QValuesResponse>();
-		ratesResponses = new ArrayList<RatesResponse>();
-		methodsToSimulate = new ArrayList<Method>();
-		epsilonDecrements = new ArrayList<String>();
-		qValuesResponses.add(QValuesResponse.DELETE_OBSOLETE_VALUES);
-		ratesResponses.add(RatesResponse.SET_TO_MIDPOINT);
-		methodsToSimulate.add(Method.QLEARNING);
-		lastValuesToCheck.add(5);
-		
-		epsilonDecrements.add("0.0008");
+		qValuesResponse = QValuesResponse.DELETE_OBSOLETE_VALUES;
+		ratesResponse = RatesResponse.SET_TO_MIDPOINT;
+		methodToSimulate = Method.QLEARNING;
+		lastValuesToCheck = new Integer(5);
+		epsilonDecrement = "0.0008";
 		
 		ACRSTRUtil.initialize();
 		puList = new ArrayList<PrimaryUser>();
@@ -71,23 +65,15 @@ public class ACRSTRSimulation {
 			oldOutput.renameTo(new File("acrstr-"
 					+ System.currentTimeMillis()));
 		}
-			
 		String output = ACRSTRUtil.getSetting("output");
-		
-		for (QValuesResponse qvr : qValuesResponses) {
-			for (RatesResponse rsr : ratesResponses) {
-				for (Method m : methodsToSimulate) {
-					for (String d : epsilonDecrements) {
-						conductSimulation(m, qvr, rsr, output, d);
-					}
-				}
-			}
-		}
+		conductSimulation(methodToSimulate, qValuesResponse, ratesResponse,
+				output, epsilonDecrement, lastValuesToCheck);
 
 	}
 	
 	public void conductSimulation(Method method, QValuesResponse qValueResponse,
-			RatesResponse ratesResponse, String output, String epsilonDecrement)
+			RatesResponse ratesResponse, String output, String epsilonDecrement,
+			Integer lastValuesToCheck)
 			throws IOException {
 		List<Double> lastNAverages = new ArrayList<Double>();
 		List<Double> lastNProbabilities = new ArrayList<Double>();
@@ -98,143 +84,128 @@ public class ACRSTRSimulation {
 		
 		int numberOfIterations = Integer.parseInt(ACRSTRUtil.getSetting("iterations"));
 		
-		for (Integer n : lastValuesToCheck) {
-			String filename = DIRECTORY_FOR_LATEST_OUTPUT + '/' + System.currentTimeMillis() + ".txt";
-			BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
-			Map<String, String> parameters = new HashMap<String, String>();
-			parameters.put("method", method.toString());
-			parameters.put("checked recent values", n.toString());
-			parameters.put("q response", qValueResponse.toString());
-			parameters.put("rate response", ratesResponse.toString());
-			parameters.put("color", colors.pop());
-			parameters.put("epsilon decrement", epsilonDecrement);
-			parameters.put("comparing", parameters.get(ACRSTRUtil.getSetting("compare")));
-			parameters.put("xLabel", ACRSTRUtil.getSetting("x-label"));
-			parameters.put("yLabel", ACRSTRUtil.getSetting("y-label"));
+		String filename = DIRECTORY_FOR_LATEST_OUTPUT + '/' + System.currentTimeMillis() + ".txt";
+		BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("method", method.toString());
+		parameters.put("checked recent values", lastValuesToCheck.toString());
+		parameters.put("q response", qValueResponse.toString());
+		parameters.put("rate response", ratesResponse.toString());
+		parameters.put("color", colors.pop());
+		parameters.put("epsilon decrement", epsilonDecrement);
+		parameters.put("comparing", parameters.get(ACRSTRUtil.getSetting("compare")));
+		parameters.put("xLabel", ACRSTRUtil.getSetting("x-label"));
+		parameters.put("yLabel", ACRSTRUtil.getSetting("y-label"));
 			
-			int numberOfLines = output.startsWith("average-of-last-n-")
-					? numberOfIterations / TAKE_AVERAGE_OF_N_VALUES : numberOfIterations;
+		int numberOfLines = output.startsWith("average-of-last-n-")
+				? numberOfIterations / TAKE_AVERAGE_OF_N_VALUES : numberOfIterations;
 			
-			parameters.put("numberOfValues", Integer.toString(numberOfLines)); 
-			
-			String jsonString = JSONValue.toJSONString(parameters);
-			bw.write(jsonString + "\n"); 
+		parameters.put("numberOfValues", Integer.toString(numberOfLines)); 
 
-			for (int i = 0; i < environment.numberOfSecondaryUsers; i++) {
-				environment.cognitiveRadios.add(new CognitiveRadio("CR" + (i + 1), environment, method,
-						n, qValueResponse, ratesResponse, Double.parseDouble(epsilonDecrement)));
-			}
-			
-			double cumulativeRewards = 0.0;
-			
-			double cumulativeSuccessProbabilities = 0.0;
-			
-			for (int i = 0; i < numberOfIterations; i++) {
-				int index = 0;
-				for (int activationEpoch : EPOCHS_TO_ACTIVATE_PU_PAIRS) {
-					if (i == activationEpoch) {
-						String puName = String.format("PU%s", index + 1);
-						environment.introduceAPU(puName);
-					}
-					index++;
-				}
-				
-				for (int deactivationEpoch : EPOCHS_TO_DEACTIVATE_PU_PAIRS) {
-					if (i == deactivationEpoch) {
-						environment.deactivateAPU();
-					}
-				}
+		String jsonString = JSONValue.toJSONString(parameters);
+		bw.write(jsonString + "\n"); 
 
-				double currentRewardTotals = 0.0;
-				
-				for (CognitiveRadio cr : environment.cognitiveRadios) {
-					cr.act();
-				}
-
-				int numberOfSuccessfulTransmissionsThisIteration = 0;
-				int channelChangesThisIteration = 0;
-				
-				for (CognitiveRadio cr : environment.cognitiveRadios) {
-					cr.evaluate();
-					currentRewardTotals += cr.currentIterationsReward;
-					if (cr.successfullyTransmittedThisIteration) {
-						numberOfSuccessfulTransmissionsThisIteration++;
-						}
-					if (cr.changedChannelThisIteration) {
-						channelChangesThisIteration++;
-					}
-				}
-				
-				cumulativeRewards += currentRewardTotals;
-				double cumulativeRewardAverage = cumulativeRewards / (i + 1);
-
-				double probabilityOfSuccessfulTransmission = (double) numberOfSuccessfulTransmissionsThisIteration
-						/ environment.numberOfSecondaryUsers;
-				
-				cumulativeSuccessProbabilities += probabilityOfSuccessfulTransmission
-						/ ((i + 1) * environment.numberOfSecondaryUsers) ;
-				
-				for (Spectrum s : environment.spectra) {
-					s.occupyingAgents.clear();
-				}
-
-				double currentRewardAverage = currentRewardTotals
-						/ environment.numberOfSecondaryUsers;
-				if (output.equals("average")) {
-					bw.write(Double.toString(currentRewardAverage) + "\n");	
-				} else if (output.equals("probability")) {
-					bw.write(Double.toString(probabilityOfSuccessfulTransmission)
-							+ "\n");
-				} else if (output.equals("cumulative-reward")) {
-					bw.write(Double.toString(cumulativeRewardAverage) + "\n");
-				} else if (output.equals("cumulative-probability")) {
-					bw.write(Double.toString(cumulativeSuccessProbabilities) + "\n");
-				} else if (output.equals("channel-changes")) {
-					bw.write(Integer.toString(channelChangesThisIteration) + "\n");
-				} else if (output.equals("average-of-last-n-rewards")) {
-					if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
-						double sumOfLastNValues = 0.0;
-						for (Double value : lastNAverages) {
-							sumOfLastNValues += value;
-						}
-						double average = sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
-						bw.write(Double.toString(average) + "\n");
-						lastNAverages.clear();
-					} else {
-						lastNAverages.add(currentRewardAverage);
-					}
-				} else if (output.equals("average-of-last-n-probabilities")) {
-					if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
-						double sumOfLastNValues = 0.0;
-						for (Double value : lastNProbabilities) {
-							sumOfLastNValues += value;
-						}
-						double average = sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
-						bw.write(Double.toString(average) + "\n");
-						lastNProbabilities.clear();
-					} else {
-						lastNProbabilities.add(probabilityOfSuccessfulTransmission);
-					}
-					
-				} else if (output.equals("average-of-last-n-channel-changes")) {
-					if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
-						double sumOfLastNValues = 0.0;
-						for (Integer value : lastNChannelChanges) {
-							sumOfLastNValues += value;
-						}
-						double average = (double) sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
-						bw.write(Double.toString(average) + "\n");
-						lastNChannelChanges.clear();
-					} else {
-						lastNChannelChanges.add(channelChangesThisIteration);
-					}
-					
-				} 
-			}
-
-			bw.close();
-
+		for (int i = 0; i < environment.numberOfSecondaryUsers; i++) {
+			environment.cognitiveRadios.add(new CognitiveRadio("CR" + (i + 1),
+					environment, method, lastValuesToCheck, qValueResponse,
+					ratesResponse, Double.parseDouble(epsilonDecrement)));
 		}
+			
+		for (int i = 0; i < numberOfIterations; i++) {
+			int index = 0;
+			for (int activationEpoch : EPOCHS_TO_ACTIVATE_PU_PAIRS) {
+				if (i == activationEpoch) {
+					String puName = String.format("PU%s", index + 1);
+					environment.introduceAPU(puName);
+				}
+				index++;
+			}
+
+			for (int deactivationEpoch : EPOCHS_TO_DEACTIVATE_PU_PAIRS) {
+				if (i == deactivationEpoch) {
+					environment.deactivateAPU();
+				}
+			}
+
+			double currentRewardTotals = 0.0;
+
+			for (CognitiveRadio cr : environment.cognitiveRadios) {
+				cr.act();
+			}
+
+			int numberOfSuccessfulTransmissionsThisIteration = 0;
+			int channelChangesThisIteration = 0;
+
+			for (CognitiveRadio cr : environment.cognitiveRadios) {
+				cr.evaluate();
+				currentRewardTotals += cr.currentIterationsReward;
+				if (cr.successfullyTransmittedThisIteration) {
+					numberOfSuccessfulTransmissionsThisIteration++;
+				}
+				if (cr.changedChannelThisIteration) {
+					channelChangesThisIteration++;
+				}
+			}
+
+			double probabilityOfSuccessfulTransmission = (double) numberOfSuccessfulTransmissionsThisIteration
+					/ environment.numberOfSecondaryUsers;
+
+			for (Spectrum s : environment.spectra) {
+				s.occupyingAgents.clear();
+			}
+
+			double currentRewardAverage = currentRewardTotals
+					/ environment.numberOfSecondaryUsers;
+			if (output.equals("average")) {
+				bw.write(Double.toString(currentRewardAverage) + "\n");	
+			} else if (output.equals("probability")) {
+				bw.write(Double.toString(probabilityOfSuccessfulTransmission)
+						+ "\n");
+			} else if (output.equals("channel-changes")) {
+				bw.write(Integer.toString(channelChangesThisIteration) + "\n");
+			} else if (output.equals("average-of-last-n-rewards")) {
+				if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
+					double sumOfLastNValues = 0.0;
+					for (Double value : lastNAverages) {
+						sumOfLastNValues += value;
+					}
+					double average = sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
+					bw.write(Double.toString(average) + "\n");
+					lastNAverages.clear();
+				} else {
+					lastNAverages.add(currentRewardAverage);
+				}
+			} else if (output.equals("average-of-last-n-probabilities")) {
+				if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
+					double sumOfLastNValues = 0.0;
+					for (Double value : lastNProbabilities) {
+						sumOfLastNValues += value;
+					}
+					double average = sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
+					bw.write(Double.toString(average) + "\n");
+					lastNProbabilities.clear();
+				} else {
+					lastNProbabilities.add(probabilityOfSuccessfulTransmission);
+				}
+
+			} else if (output.equals("average-of-last-n-channel-changes")) {
+				if (i % TAKE_AVERAGE_OF_N_VALUES == 0) {
+					double sumOfLastNValues = 0.0;
+					for (Integer value : lastNChannelChanges) {
+						sumOfLastNValues += value;
+					}
+					double average = (double) sumOfLastNValues / TAKE_AVERAGE_OF_N_VALUES;
+					bw.write(Double.toString(average) + "\n");
+					lastNChannelChanges.clear();
+				} else {
+					lastNChannelChanges.add(channelChangesThisIteration);
+				}
+
+			} 
+		}
+
+		bw.close();
+
 	}
 
 }
