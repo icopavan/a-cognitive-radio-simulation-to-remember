d42c65813cd0fe0f6bee07a67ac702db785972c1
femnad <femnad@gmail.com>
2013-01-10 17:43:08 +0200

    something something something

--- src/ACRSTRRunner.java
+++ src/ACRSTRRunner.java
@@ -18,20 +18,20 @@ public class ACRSTRRunner {
 				0.0008, 5, QValuesResponse.DELETE_OBSOLETE_VALUES,
 				RatesResponse.SET_TO_MIDPOINT, "blue");
 		ACRSTRSimulation simulation2 = new ACRSTRSimulation(Method.QLEARNING,
-				0.0008, 0, QValuesResponse.DELETE_OBSOLETE_VALUES,
-				RatesResponse.SET_TO_MIDPOINT, "red");
-/*		ACRSTRSimulation simulation3 = new ACRSTRSimulation(Method.GREEDY,
-				0.0008, 0, QValuesResponse.DELETE_OBSOLETE_VALUES,
-				RatesResponse.SET_TO_MIDPOINT, "green");
-*/		try {
+				0.0008, 5, QValuesResponse.DELETE_OBSOLETE_VALUES,
+				RatesResponse.INCREASE_BY_CONSTANT, "red");
+		ACRSTRSimulation simulation3 = new ACRSTRSimulation(Method.QLEARNING,
+				0.0008, 5, QValuesResponse.DELETE_OBSOLETE_VALUES,
+				RatesResponse.RESET_TO_INITIAL_VALUES, "green");
+		try {
 			simulation1.startSimulation();
 			simulation2.startSimulation();
-//			simulation3.startSimulation();	
+			simulation3.startSimulation();	
 			BufferedWriter infoBW = new BufferedWriter
 					(new FileWriter(ACRSTRSimulation.DIRECTORY_FOR_LATEST_OUTPUT + "/info.ini"));
 			infoBW.write(simulation1.parameters.toString() + "\n");
 			infoBW.write(simulation2.parameters.toString() + "\n");
-//			infoBW.write(simulation3.parameters.toString() + "\n");
+			infoBW.write(simulation3.parameters.toString() + "\n");
 			infoBW.close();
 		} catch (IOException e) {
 			e.printStackTrace();
